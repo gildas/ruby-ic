431cdbf8b28414917b2182639da8b9433fe64873
Gildas Cherruel <gildas@breizh.org>
2014-07-25 02:32:11 +0900

    Added missing HTTP statuses

--- lib/ic/http_statuses.rb
+++ lib/ic/http_statuses.rb
@@ -3,6 +3,22 @@
 module Ic
   module HTTP
     module Status
+      OK                              = 200
+      CREATED                         = 201
+      ACCEPTED                        = 202
+      NON_AUTHORITATIVE_INFORMATION   = 203
+      NO_CONTENT                      = 204
+      RESET_CONTENT                   = 205
+      PARTIAL_CONTENT                 = 206
+
+      MOVED_PERMANENTLY               = 301
+      FOUND                           = 302
+      SEE_OTHER                       = 303
+      TEMPORARY_REDIRECT              = 307
+      MOVED_TEMPORARILY               = 307
+
+      BAD_REQUEST                     = 400
+      UNAUTHORIZED                    = 401
       FORBIDDEN                       = 403
       NOT_FOUND                       = 404
       METHOD_NOT_ALLOWED              = 405
@@ -14,7 +30,7 @@ module Ic
       LENGTH_REQUIRED                 = 411
       PRECONDITION_FAILED             = 412
 
-
+      INTERNAL                        = 500
       NOT_IMPLEMENTED                 = 501
       BAD_GATEWAY                     = 502
       SERVICE_UNAVAILABLE             = 503
