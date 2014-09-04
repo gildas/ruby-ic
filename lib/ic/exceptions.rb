2d38e736f220a17bb8334425b47f3a8eef5e4833
Gildas Cherruel <gildas.cherruel@inin.com>
2014-09-04 23:19:25 +0900

    Added documentation to exceptions

--- lib/ic/exceptions.rb
+++ lib/ic/exceptions.rb
@@ -1,12 +1,13 @@
 module Ic
-# This exception is raised when a method is expecting a parameter, but it was missing
+  # This exception is raised when a method is expecting a parameter, but it was missing
   class MissingArgumentError < ArgumentError ; end
 
-# This exception is raised when a method is expecting a parameter, but it is invalid
+  # This exception is raised when a method is expecting a parameter, but it is invalid
   class InvalidArgumentError < ArgumentError ; end
 
-# This exception is raised when an object is instanciated from JSON but has the wrong __type
+  # This exception is raised when an object is instanciated from JSON but has the wrong __type
   class InvalidTypeError < ArgumentError ; end
 
+  # This exception is raised when connecting to a station that does not exist
   class StationNotFoundError < IndexError ; end
 end
--- lib/ic/http_exceptions.rb
+++ lib/ic/http_exceptions.rb
@@ -1,26 +1,36 @@
 module Ic
   module HTTP
+    # This is the generic exception for HTTP Errors
     class HTTPError < RuntimeError ; end
 
+    # This Exception is raised when the {Client} receives a request to redirect from the server 
     class WantRedirection < HTTPError ; end
 
+    # This Exception is raised when the server is not able to serve the {Client}
     class UnavailableService < HTTPError ; end
 
+    # This Exception is raised when the {Client} did not formulate its request properly
     class BadRequestError < HTTPError ; end
 
+    # This Exception is raised when the {Client} asked for a resource that does not exist
     class NotFoundError < HTTPError ; end
 
+    # This Exception is raised when the server cannot authenticate the {Client}
     class AuthenticationError < HTTPError ; end
 
+    # This Exception is raised when the {Client} is not authorized on the server
     class UnauthorizedError < HTTPError ; end
 
+    # This Exception is raised when the {Client} used a {Session} that is expired
     class SessionIdExpectedError < UnauthorizedError ; end
 
+    # This Exception is raised when the {Client} did not provide a token in its request
     class AuthTokenExpectedError < UnauthorizedError ; end
 
+    # This Exception is raised when the {Client} used an invalid {Session}
     class InvalidSessionIdError < UnauthorizedError ; end
 
+    # This Exception is raised when the {Client} received too many redirection requests
     class TooManyRedirectionsError < HTTPError ; end
-
   end
 end
