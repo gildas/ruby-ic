eca3366ec6d9776858c97ad49c434dbd951d8ba2
Gildas Cherruel <gildas.cherruel@inin.com>
2014-09-04 23:05:28 +0900

    Added documentation and using keywork arguments

--- lib/ic/country.rb
+++ lib/ic/country.rb
@@ -1,11 +1,22 @@
 module Ic
+  # This class represents country information
+  #
+  # @see http://www.iso.org/iso/country_codes.htm Country codes (ISO 3166)
   class Country
-    attr_reader :id, :name
+    # return [String] The country identifier
+    attr_reader :id
 
-    def initialize(options = {})
-      raise MissingArgumentError, 'id' unless options[:id]
-      @id   = options[:id]
-      @name = options[:name] || options[:displayName]
+    # return [String] The country name
+    attr_reader :name
+
+    # Initializes a new Country
+    # @overload initialize(id,name)
+    #   @param id          [String] The country identifier (ISO 3166)
+    #   @param name        [String] A human readable name (e.g.: 'France')
+    #   @param displayName [String] A synonym for parameter name
+    def initialize(id:, name: nil, displayName: nil)
+      @id   = id
+      @name = name || displayName || @id # IC API key: displayName
     end
   end
 end
