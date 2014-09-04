bf29942e6e90e9fb7f658fe4413d723fcc9c90b6
Gildas Cherruel <gildas.cherruel@inin.com>
2014-09-05 00:17:56 +0900

    Added documentation about License

--- lib/ic/license.rb
+++ lib/ic/license.rb
@@ -4,11 +4,18 @@ require 'ic/exceptions'
 require 'ic/logger'
 
 module Ic
+  # This class represents a Licence for a CIC object
+  # It tells what the server can and cannot do.
   class License
     include Traceable
 
-    attr_reader :id, :status
+    # @return [String] The license identifier
+    attr_reader :id
 
+    # @return [Status] The license status.
+    attr_reader :status
+
+    # All statuses of a {License}
     module Status
       UNKNOWN_ERROR                = 0
       NONE                         = 1
@@ -21,11 +28,17 @@ module Ic
       OTHER_APPLICATION            = 8
     end
 
-    def initialize(**options)
-      raise MissingArgumentError, 'session' unless (@session = options[:session])
+    # Initializes a License
+    # @param id [String] The Identifier of the License
+    # @param licenseName [String] The Identifier of the License (when returned from CIC)
+    # @param status [String] The current status of the license
+    # @param licenseStatus [String] The current status of the license (when returned from CIC)
+    # @raise [MissingArgumentError] if id and licenseName are nil
+    #                               if status and licenseStatus are nil
+    def initialize(session: session, id: nil, licenseName: nil, status: nil, licenseStatus: nil, **options)
       self.create_logger(**options, default: @session)
-      raise MissingArgumentError, 'id'     unless (@id     = options[:id] || options[:licenseName])
-      raise MissingArgumentError, 'status' unless (@status = options[:status] || options[:licenseStatus])
+      raise MissingArgumentError, 'id'     unless (@id     = id || licenseName)
+      raise MissingArgumentError, 'status' unless (@status = status || licenseStatus)
       if options.include?(:available)
         @available = options[:available]
       elsif options.include?(:isAvailable)
@@ -36,6 +49,7 @@ module Ic
       # TODO: Add processing for :errorDetails (which has the value of " none" upon success)
     end
 
+    # @returns [Boolean] Tells if a {License}  is available or not.
     def available? ; @available end
 
   end
