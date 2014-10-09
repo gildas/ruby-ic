a578d5a4c269f2794e4cc4b67666aee724d44c3a
Gildas Cherruel <gildas.cherruel@inin.com>
2014-10-09 14:45:47 +0900

    Added documentation to modified standard classes

--- lib/ic/helpers.rb
+++ lib/ic/helpers.rb
@@ -1,4 +1,14 @@
+# Additional methods for the Hash class.
+#
+# @see {::Hash} for general documentation about Hash in Ruby.
 class Hash
+  # This method takes a Hash with string keys and gives a Hash with symbol keys.
+  #
+  # @example
+  #   foo = { "myKey": "value", "myKeys": [ "value", "myInnerKey": "value" ] }
+  #   => { my_key: "value", my_keys: [ "value", my_inner_key: "value" ] }
+  #
+  # @return [Hash] The resulting Hash
   def keys2sym
     keys2sym = lambda do |h|
       case h
@@ -11,7 +21,17 @@ class Hash
   end
 end
 
+# Additional methods for the Array class.
+#
+# @see {::Array} for general documentation about Array in Ruby.
 class Array
+  # This method takes an Array with pairs having string keys and gives a Array where the pairs use symbol keys.
+  #
+  # @example
+  #   foo = [ "value", "myKey": "value" ]
+  #   => [ "value", my_key: "value" ]
+  #
+  # @return [Array] The resulting Array
   def keys2sym
     self.collect do |item|
       case item
